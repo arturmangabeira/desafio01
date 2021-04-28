45cab7ec9dc7bb621b24564d38fc9e4e6e6f7187
arturmangabeira <arturmangabeira@gmail.com>
2021-04-27 22:48:44 -0300

    ajustes

--- mysql/scripts/CreateDatabase.sql
+++ mysql/scripts/CreateDatabase.sql
@@ -1,19 +1,11 @@
-
-
-create database desafio01;
-
 use desafio01;
 
 CREATE TABLE modulo_cursos (
-    id integer not null ,
+    id integer not null auto_increment PRIMARY KEY,
     ativo boolean not null,
     nome varchar(50) not null,
     descricao varchar(2000) not null    
 );
-
-ALTER TABLE modulo_cursos  ADD PRIMARY KEY (id);
-
-ALTER TABLE modulo_cursos MODIFY id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 SET character_set_client = utf8;
 SET character_set_connection = utf8;
@@ -27,8 +19,6 @@ INSERT INTO modulo_cursos (ativo, nome, descricao) VALUES (1, 'DDD', 'Curso de D
 INSERT INTO modulo_cursos (ativo, nome, descricao) VALUES (1, 'MicroServiço: Laravel (Back-end)', 'Curso de Laravel');
 INSERT INTO modulo_cursos (ativo, nome, descricao) VALUES (1, 'MicroServiço: React (Front-End)', 'Curso de React');
 
---CRIA O USER PARA ACESSAR AO BANCO DE DADOS PELA APLICACAO EXTERNA.
+--CREATE USER 'userdesafio'@'%' IDENTIFIED BY 'desafio01';
 
-CREATE USER 'userdesafio'@'%' IDENTIFIED BY 'desafio01';
-
-GRANT ALL PRIVILEGES ON *.* TO 'userdesafio'@'%';
+--GRANT ALL PRIVILEGES ON *.* TO 'userdesafio'@'%';
